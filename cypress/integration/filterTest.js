017a63ce4ff6d8078017179c637a00147fc09bad
KonstRk <qasenior32@gmail.com>
2019-06-02 12:06:37 +0300

    Rename scenarios

--- cypress/integration/filterTest.js
+++ cypress/integration/filterTest.js
@@ -6,21 +6,21 @@ describe('Test cases', function() {
         page.openPage();
     });
 
-    it('First scenario', function() {
+    it('Scenario 1: DSL Result List - verify result list', function() {
         page.selectDSL();
         page.typeToCalculator('030');
         page.startSearch();
         page.validateList();
     });
 
-    it('Second scenario', function() {
+    it('Scenario 2: Result List - verify Offer detail page', function() {
         page.selectDSL();
         page.typeToCalculator('030');
         page.startSearch();
         page.validateFirstTarif();
     });
 
-    it('Third scenario', function() {
+    it('Scenario 3: Lazy loading/pagination for loading the Result List', function() {
         page.selectDSL();
         page.typeToCalculator('030');
         page.startSearch();
